5e2c14a2aa9cb1e7ea21d919053cfe8ba2c5e79d
Family <sabziamjad@gmail.com>
2018-05-20 21:50:02 +0100

    changes color of random cell

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 $(function() {
 
   const cellsArray = ["cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6"];
-let randomCell = cellsArray[Math.floor(Math.random()*cellsArray.length)];
-console.log(randomCell);
+  let randomCell = cellsArray[Math.floor(Math.random()*cellsArray.length)];
+
+  $('#'+ randomCell).css('background-color', 'yellow');
 
 });
