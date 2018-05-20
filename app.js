7d1013cf5da9acca6654621be35080021845f5a7
Family <sabziamjad@gmail.com>
2018-05-20 21:42:37 +0100

    Selects random cell from table

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Self invoking function
 $(function() {
 
+  const cellsArray = ["cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6"];
+let randomCell = cellsArray[Math.floor(Math.random()*cellsArray.length)];
+console.log(randomCell);
 
-
-})
+});
