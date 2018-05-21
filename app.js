2a1dc488e5bcefac6326f3bfc6c616a8ffcfa95b
Family <sabziamjad@gmail.com>
2018-05-21 12:19:46 +0100

    Does not repeat random cell selected twice

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 // Self invoking function
+const cellsArray = ['cell-1', 'cell-2', 'cell-3', 'cell-4', 'cell-5', 'cell-6'];
+
+function selectRandomCells(){
+  const index = Math.floor(Math.random()*cellsArray.length);
+  const randomCell = cellsArray[index];
+  $('#'+ randomCell).addClass('yellow');
+  cellsArray.splice(index, 1);
+  console.log(cellsArray);
+}
+
 $(function() {
-
-  const cellsArray = ["cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6"];
-
   // Run the computer generating logic only two times
-  for (i = 1; i < 3; i++) {
-
+  for (let i = 1; i < 2; i++) {
     // This function will run 2 times
     // It will pick random cell and change its background color to yellow
-    function selectRandomCells(){
-
-      let randomCell = cellsArray[Math.floor(Math.random()*cellsArray.length)];
-      $('#'+ randomCell).css('background-color', 'yellow');
-
-    }
-
+    selectRandomCells();
     // First time i = 1, (1 * 1000) which means run first colour change on 1 second
     // Second time i = 2 (2 * 2000) which means run second colour change on 2nd second
 
-    setTimeout(selectRandomCells, i * 1000)
-
+    setTimeout(selectRandomCells, i * 1000);
 
   }
 
