6fde404aec0699bcee7791c12f64dd66c51ab47d
Family <sabziamjad@gmail.com>
2018-05-22 09:23:52 +0100

    defined a function to remove yellow cells after flashing

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-// Self invoking function
 const cellsArray = ['cell-1', 'cell-2', 'cell-3', 'cell-4', 'cell-5', 'cell-6'];
 
+// This function selects a random cell from the above cells, and turns it yellow
 function selectRandomCell(){
+
+  // Get a random index from cells array
   const index = Math.floor(Math.random()*cellsArray.length);
+  // Find that cell value through the index i.e array[index]
   const randomCell = cellsArray[index];
+
+  // Add Yellow class to it
   $('#'+ randomCell).addClass('yellow');
+
+  // Remove it from collection so we don't pick it again
   cellsArray.splice(index, 1);
-  console.log(cellsArray);
+
 }
 
+// Self invoking function
 $(function() {
-  // Run the computer generating logic only two times
+
+
+  // When a user clicks start button
   document.getElementById('start-button').onclick = function() {
-    for (let i = 1; i < 7; i++) {
+
+    // Do this 3 times
+    for (let i = 1; i < 4; i++) {
+
+      // Run selectRandomCell function per second
       setTimeout(selectRandomCell, i * 1000);
+
+      // Remove the yellow colour and clear the cells
+      setTimeout(function(){
+        $('td').removeClass('yellow');
+      }, 4000);
+
     }
+
   };
+
 });
